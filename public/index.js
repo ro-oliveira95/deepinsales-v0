26eb8b3f45d3843fdb973b0480ae20160b923097
ro-oliveira95 <ro.oliveira.neto95@gmail.com>
2020-11-28 11:54:47 -0300

    improving chart feature

--- public/index.js
+++ public/index.js
@@ -1,103 +1,62 @@
 document.querySelector(".add-ad").addEventListener("submit", addNewAdToDB);
 document.addEventListener("DOMContentLoaded", loadAds);
-document.addEventListener("DOMContentLoaded", renderPlot);
+document.addEventListener("DOMContentLoaded", createChart);
 // document.addEventListener("DOMContentLoaded", connectEvents);
 
 // init global variables
 let chart;
 
-function renderPlot() {
-  chart = createNewChart("gráfico 1");
-  chart.render();
-  chart._updateSize()
-  // chart.update();
-  // ch = document.getElementById('chartContainer').style.width = "200px";
+function updateChart(data) {
+  data.forEach((dataset, index) => {
+    //chart.data.labels = ["i1", "i2", "i3", "i4", "i5"];
+    chart.data.datasets.push(dataset);
+  });
+  chart.update();
 }
 
-function connectEvents() {
-  document.querySelector(".btn-details").addEventListener("click", (e) => {
-    document.querySelector(".faded-background").style.display = "flex";
+function createChart() {
+  var ctx = document.getElementById("chart");
+  chart = new Chart(ctx, {
+    type: "line",
+    data: {
+      datasets: [],
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: true,
+            },
+          },
+        ],
+        xAxes: [
+          {
+            type: "time",
+            display: true,
+            time: {
+              unit: "seconds",
+            },
+          },
+        ],
+      },
+    },
   });
-  document.querySelector(".faded-background").addEventListener("click", (e) => {
-    if (e.target.className == "faded-background") {
-      document.querySelector(".faded-background").style.display = "none";
-    }
-  });
-}
 
-function createNewChart(title) {
-  let canvas = new CanvasJS.Chart("chartContainer", {
-    responsive: true,
-    maintainAspectRatio: false,
-    animationEnabled: false,
-    title: {
-      text: title,
-    },
-    axisX: {
-      valueFormatString: "DD MMM,YY",
-    },
-    axisY: {
-      title: "Temperature (in °C)",
-      suffix: " °C",
-    },
-    legend: {
-      cursor: "pointer",
-      fontSize: 16,
-      itemclick: toggleDataSeries,
-    },
-    toolTip: {
-      shared: true,
-    },
-    data: [
-      {
-        name: "Myrtle Beach",
-        type: "spline",
-        yValueFormatString: "#0.## °C",
-        showInLegend: true,
-        dataPoints: [
-          { x: new Date(2017, 6, 24), y: 31 },
-          { x: new Date(2017, 6, 25), y: 31 },
-          { x: new Date(2017, 6, 26), y: 29 },
-          { x: new Date(2017, 6, 27), y: 29 },
-          { x: new Date(2017, 6, 28), y: 31 },
-          { x: new Date(2017, 6, 29), y: 30 },
-          { x: new Date(2017, 6, 30), y: 29 },
-        ],
-      },
-      {
-        name: "Martha Vineyard",
-        type: "spline",
-        yValueFormatString: "#0.## °C",
-        showInLegend: true,
-        dataPoints: [
-          { x: new Date(2017, 6, 24), y: 20 },
-          { x: new Date(2017, 6, 25), y: 20 },
-          { x: new Date(2017, 6, 26), y: 25 },
-          { x: new Date(2017, 6, 27), y: 25 },
-          { x: new Date(2017, 6, 28), y: 25 },
-          { x: new Date(2017, 6, 29), y: 25 },
-          { x: new Date(2017, 6, 30), y: 25 },
-        ],
-      },
-      {
-        name: "Nantucket",
-        type: "spline",
-        yValueFormatString: "#0.## °C",
-        showInLegend: true,
-        dataPoints: [
-          { x: new Date(2017, 6, 24), y: 22 },
-          { x: new Date(2017, 6, 25), y: 19 },
-          { x: new Date(2017, 6, 26), y: 23 },
-          { x: new Date(2017, 6, 27), y: 24 },
-          { x: new Date(2017, 6, 28), y: 24 },
-          { x: new Date(2017, 6, 29), y: 23 },
-          { x: new Date(2017, 6, 30), y: 23 },
-        ],
-      },
-    ],
-  });
-  console.log(canvas);
-  return canvas;
+  function connectEvents() {
+    document.querySelector(".btn-details").addEventListener("click", (e) => {
+      document.querySelector(".faded-background").style.display = "flex";
+    });
+    document
+      .querySelector(".faded-background")
+      .addEventListener("click", (e) => {
+        if (e.target.className == "faded-background") {
+          document.querySelector(".faded-background").style.display = "none";
+        }
+      });
+  }
 }
 
 function loadAds() {
@@ -128,7 +87,7 @@ function loadAds() {
             <div class="btn-container">
               <button class="btn btn-details">+ detalhes</button>
               <label class="switch">
-                <input type="checkbox">
+                <input type="checkbox" checked="true">
                 <span class="slider round"></span>
               </label>
             </div>
@@ -138,7 +97,50 @@ function loadAds() {
       });
 
       adDisplay.innerHTML = adsListHTML;
-      connectEvents();
+
+      let data = [];
+
+      ads.forEach((ad) => {
+        const formatedDate = new Date(ad.createdAt);
+        const datetime = formatedDate
+          .toISOString()
+          .slice(0, 10)
+          .replace(/-/g, "");
+
+        series = {
+          label: ad.name,
+          data: [
+            {
+              x: datetime,
+              y: 10,
+            },
+          ],
+        };
+
+        console.log(ad.createdAt);
+
+        data.push(series);
+      });
+
+      /*let data = [
+        {
+          label: "s1",
+          borderColor: "blue",
+          data: [
+            { x: "2017-01-06 18:39:30", y: 100 },
+            { x: "2017-01-07 18:39:28", y: 101 },
+          ],
+        },
+        {
+          label: "s2",
+          borderColor: "red",
+          data: [
+            { x: "2017-01-07 18:00:00", y: 90 },
+            { x: "2017-01-08 18:00:00", y: 105 },
+          ],
+        },
+      ]; */
+      updateChart(data);
     })
     .catch((err) => console.log(err));
 }
